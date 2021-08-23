22632d4d2fd661673850cfbb293b1f9ec0bab969
Saeed Salehlou <Salehlou@hotmail.com>
2021-08-23 13:43:34 +0430

    change data

--- src/app/_mock-data/flat-tree-data.ts
+++ src/app/_mock-data/flat-tree-data.ts
@@ -1,7 +1,7 @@
 export const FLAT_TREE_DATA = {
    "data": [
       {
-         checked: true,
+         checked: false,
          iconId: '',
          id: 1,
          name: "رابط کاربری",
@@ -16,46 +16,46 @@ export const FLAT_TREE_DATA = {
          parentId: 1,
          write: false
       },
-      {
-         checked: false,
-         iconId: '',
-         id: 3,
-         name: "زیر سیستم ها",
-         parentId: 1,
-         write: false
-      },
-      {
-         checked: false,
-         iconId: '',
-         id: 4,
-         name: "سطوح دسترسی و کاربران",
-         parentId: 1,
-         write: false
-      },
-      {
-         checked: false,
-         iconId: '',
-         id: 5,
-         name: "تنظیمات",
-         parentId: 1,
-         write: false
-      },
-      {
-         checked: false,
-         iconId: '',
-         id: 6,
-         name: "گزارشات",
-         parentId: 1,
-         write: false
-      },
-      {
-         checked: false,
-         iconId: '',
-         id: 7,
-         name: "تنظیمات کاربری",
-         parentId: 1,
-         write: false
-      },
+      // {
+      //    checked: false,
+      //    iconId: '',
+      //    id: 3,
+      //    name: "زیر سیستم ها",
+      //    parentId: 1,
+      //    write: false
+      // },
+      // {
+      //    checked: false,
+      //    iconId: '',
+      //    id: 4,
+      //    name: "سطوح دسترسی و کاربران",
+      //    parentId: 1,
+      //    write: false
+      // },
+      // {
+      //    checked: false,
+      //    iconId: '',
+      //    id: 5,
+      //    name: "تنظیمات",
+      //    parentId: 1,
+      //    write: false
+      // },
+      // {
+      //    checked: false,
+      //    iconId: '',
+      //    id: 6,
+      //    name: "گزارشات",
+      //    parentId: 1,
+      //    write: false
+      // },
+      // {
+      //    checked: false,
+      //    iconId: '',
+      //    id: 7,
+      //    name: "تنظیمات کاربری",
+      //    parentId: 1,
+      //    write: false
+      // },
       {
          checked: false,
          iconId: '',
@@ -88,206 +88,206 @@ export const FLAT_TREE_DATA = {
          parentId: 2,
          write: false
       },
-      {
-         checked: false,
-         iconId: '',
-         id: 12,
-         name: "اکسس کنترل",
-         parentId: 3,
-         write: false
-      },
-      {
-         checked: false,
-         iconId: '',
-         id: 13,
-         name: "ارباب رجوع",
-         parentId: 3,
-         write: false
-      },
-      {
-         checked: false,
-         iconId: '',
-         id: 14,
-         name: "صندوق امانات",
-         parentId: 3,
-         write: false
-      },
-      {
-         checked: false,
-         iconId: '',
-         id: 15,
-         name: "پارکینگ",
-         parentId: 3,
-         write: false
-      },
-      {
-         checked: false,
-         iconId: '',
-         id: 16,
-         name: "مدیریت باشگاه های ورزشی",
-         parentId: 3,
-         write: false
-      },
-      {
-         checked: false,
-         iconId: '',
-         id: 17,
-         name: "گروه های دسترسی",
-         parentId: 12,
-         write: false
-      },
-      {
-         checked: false,
-         iconId: '',
-         id: 18,
-         name: "درب ها",
-         parentId: 12,
-         write: false
-      },
-      {
-         checked: false,
-         iconId: '',
-         id: 19,
-         name: "آسانسور ها",
-         parentId: 12,
-         write: false
-      },
-      {
-         checked: false,
-         iconId: '',
-         id: 20,
-         name: "مراجعین",
-         parentId: 13,
-         write: false
-      },
-      {
-         checked: false,
-         iconId: '',
-         id: 21,
-         name: "آفیش",
-         parentId: 13,
-         write: false
-      },
-      {
-         checked: false,
-         iconId: '',
-         id: 22,
-         name: "اقلام همراه",
-         parentId: 13,
-         write: false
-      },
-      {
-         checked: false,
-         iconId: '',
-         id: 23,
-         name: "لیست سیاه",
-         parentId: 13,
-         write: false
-      },
-      {
-         checked: false,
-         iconId: '',
-         id: 24,
-         name: "کاربران",
-         parentId: 4,
-         write: false
-      },
-      {
-         checked: false,
-         iconId: '',
-         id: 25,
-         name: "نقش ها",
-         parentId: 4,
-         write: false
-      },
-      {
-         checked: false,
-         iconId: '',
-         id: 26,
-         name: "سرور",
-         parentId: 5,
-         write: false
-      },
-      {
-         checked: false,
-         iconId: '',
-         id: 27,
-         name: "سایر نرم افزار ها",
-         parentId: 5,
-         write: false
-      },
-      {
-         checked: false,
-         iconId: '',
-         id: 28,
-         name: "پایه",
-         parentId: 6,
-         write: false
-      },
-      {
-         checked: false,
-         iconId: '',
-         id: 29,
-         name: "اکسس کنترل",
-         parentId: 6,
-         write: false
-      },
-      {
-         checked: false,
-         iconId: '',
-         id: 30,
-         name: "ارباب رجوع",
-         parentId: 6,
-         write: false
-      },
-      {
-         checked: false,
-         iconId: '',
-         id: 31,
-         name: "صندوق امانات",
-         parentId: 6,
-         write: false
-      },
-      {
-         checked: false,
-         iconId: '',
-         id: 32,
-         name: "پارکینگ",
-         parentId: 6,
-         write: false
-      },
-      {
-         checked: false,
-         iconId: '',
-         id: 33,
-         name: "کاربران",
-         parentId: 6,
-         write: false
-      },
-      {
-         checked: false,
-         iconId: '',
-         id: 34,
-         name: "تغییر کلمه عبور",
-         parentId: 7,
-         write: false
-      },
-      {
-         checked: false,
-         iconId: '',
-         id: 35,
-         name: "شخصی سازی",
-         parentId: 7,
-         write: false
-      },
-      {
-         checked: false,
-         iconId: '',
-         id: 36,
-         name: "جریان کاری",
-         parentId: 13,
-         write: false
-      }
+      // {
+      //    checked: false,
+      //    iconId: '',
+      //    id: 12,
+      //    name: "اکسس کنترل",
+      //    parentId: 3,
+      //    write: false
+      // },
+      // {
+      //    checked: false,
+      //    iconId: '',
+      //    id: 13,
+      //    name: "ارباب رجوع",
+      //    parentId: 3,
+      //    write: false
+      // },
+      // {
+      //    checked: false,
+      //    iconId: '',
+      //    id: 14,
+      //    name: "صندوق امانات",
+      //    parentId: 3,
+      //    write: false
+      // },
+      // {
+      //    checked: false,
+      //    iconId: '',
+      //    id: 15,
+      //    name: "پارکینگ",
+      //    parentId: 3,
+      //    write: false
+      // },
+      // {
+      //    checked: false,
+      //    iconId: '',
+      //    id: 16,
+      //    name: "مدیریت باشگاه های ورزشی",
+      //    parentId: 3,
+      //    write: false
+      // },
+      // {
+      //    checked: false,
+      //    iconId: '',
+      //    id: 17,
+      //    name: "گروه های دسترسی",
+      //    parentId: 12,
+      //    write: false
+      // },
+      // {
+      //    checked: false,
+      //    iconId: '',
+      //    id: 18,
+      //    name: "درب ها",
+      //    parentId: 12,
+      //    write: false
+      // },
+      // {
+      //    checked: false,
+      //    iconId: '',
+      //    id: 19,
+      //    name: "آسانسور ها",
+      //    parentId: 12,
+      //    write: false
+      // },
+      // {
+      //    checked: false,
+      //    iconId: '',
+      //    id: 20,
+      //    name: "مراجعین",
+      //    parentId: 13,
+      //    write: false
+      // },
+      // {
+      //    checked: false,
+      //    iconId: '',
+      //    id: 21,
+      //    name: "آفیش",
+      //    parentId: 13,
+      //    write: false
+      // },
+      // {
+      //    checked: false,
+      //    iconId: '',
+      //    id: 22,
+      //    name: "اقلام همراه",
+      //    parentId: 13,
+      //    write: false
+      // },
+      // {
+      //    checked: false,
+      //    iconId: '',
+      //    id: 23,
+      //    name: "لیست سیاه",
+      //    parentId: 13,
+      //    write: false
+      // },
+      // {
+      //    checked: false,
+      //    iconId: '',
+      //    id: 24,
+      //    name: "کاربران",
+      //    parentId: 4,
+      //    write: false
+      // },
+      // {
+      //    checked: false,
+      //    iconId: '',
+      //    id: 25,
+      //    name: "نقش ها",
+      //    parentId: 4,
+      //    write: false
+      // },
+      // {
+      //    checked: false,
+      //    iconId: '',
+      //    id: 26,
+      //    name: "سرور",
+      //    parentId: 5,
+      //    write: false
+      // },
+      // {
+      //    checked: false,
+      //    iconId: '',
+      //    id: 27,
+      //    name: "سایر نرم افزار ها",
+      //    parentId: 5,
+      //    write: false
+      // },
+      // {
+      //    checked: false,
+      //    iconId: '',
+      //    id: 28,
+      //    name: "پایه",
+      //    parentId: 6,
+      //    write: false
+      // },
+      // {
+      //    checked: false,
+      //    iconId: '',
+      //    id: 29,
+      //    name: "اکسس کنترل",
+      //    parentId: 6,
+      //    write: false
+      // },
+      // {
+      //    checked: false,
+      //    iconId: '',
+      //    id: 30,
+      //    name: "ارباب رجوع",
+      //    parentId: 6,
+      //    write: false
+      // },
+      // {
+      //    checked: false,
+      //    iconId: '',
+      //    id: 31,
+      //    name: "صندوق امانات",
+      //    parentId: 6,
+      //    write: false
+      // },
+      // {
+      //    checked: false,
+      //    iconId: '',
+      //    id: 32,
+      //    name: "پارکینگ",
+      //    parentId: 6,
+      //    write: false
+      // },
+      // {
+      //    checked: false,
+      //    iconId: '',
+      //    id: 33,
+      //    name: "کاربران",
+      //    parentId: 6,
+      //    write: false
+      // },
+      // {
+      //    checked: false,
+      //    iconId: '',
+      //    id: 34,
+      //    name: "تغییر کلمه عبور",
+      //    parentId: 7,
+      //    write: false
+      // },
+      // {
+      //    checked: false,
+      //    iconId: '',
+      //    id: 35,
+      //    name: "شخصی سازی",
+      //    parentId: 7,
+      //    write: false
+      // },
+      // {
+      //    checked: false,
+      //    iconId: '',
+      //    id: 36,
+      //    name: "جریان کاری",
+      //    parentId: 13,
+      //    write: false
+      // }
    ],
 
 };
