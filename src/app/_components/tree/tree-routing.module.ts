df5973c51351ee766f3bc0fa61253a1612e05a58
Mehdi Payervand <mehdipayervand@outlook.com>
2021-08-24 10:08:35 +0430

    add cdk tree flat

--- src/app/_components/tree/tree-routing.module.ts
+++ src/app/_components/tree/tree-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CdkTreeFlatSimpleComponent } from './cdk-tree-flat-simple/cdk-tree-flat-simple.component';
 import { FlatComponent } from './flat/flat.component';
 import { NestedComponent } from './nested/nested.component';
 import { SampleComponent } from './sample/sample.component';
@@ -11,6 +12,7 @@ const routes: Routes = [
     component: TreeComponent,
     children: [
       { path: 'flat', component: FlatComponent, data: { returnUrl: window.location.pathname } },
+      { path: 'cdkflat', component: CdkTreeFlatSimpleComponent, data: { returnUrl: window.location.pathname } },
       { path: 'sample', component: SampleComponent, data: { returnUrl: window.location.pathname } },
       { path: 'nested', component: NestedComponent, data: { returnUrl: window.location.pathname } },
       { path: '', redirectTo: 'flat', pathMatch: 'full' },
